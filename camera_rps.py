f1a3d4c6194e8977531cb8c3867c226a7ac252f3
Venus Cheung <venus.cheung@outlook.com>
2023-02-24 13:17:16 +0000

    added timer

--- camera_rps.py
+++ camera_rps.py
@@ -1,13 +1,17 @@
 import cv2
 from keras.models import load_model
 import numpy as np
+import time
 
 def get_machine_output():
     model = load_model('keras_model.h5')
     cap = cv2.VideoCapture(0)
     data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+    max_time = 5
+    start_time = time.time()
 
-    while True: 
+    while (time.time() - start_time) < max_time: 
+    # while True:     
         ret, frame = cap.read()
         resized_frame = cv2.resize(frame, (224, 224), interpolation = cv2.INTER_AREA)
         image_np = np.array(resized_frame)
@@ -39,4 +43,4 @@ def get_prediction():
     else:
         return None
 
-print(get_prediction())
+print(f'You chose {get_prediction()}')
